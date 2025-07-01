bdd790e1ee2a324686a028de515b6620c9b47dc1
Y-K27 <yuriikarpiuk21@gmail.com>
2025-07-01 20:00:07 +0200

    The functionality of the radionbuttons for changing difficulty level and swiching the language has been fixed

--- Administrative_divisions_of_Ukraine/labels.py
+++ Administrative_divisions_of_Ukraine/labels.py
@@ -2,19 +2,27 @@ labels = {
     "ua":{
         "screen_title": "Гра області України",
         "division_name": "ua_name",
-        "screen_textinput_title": "Відгадай область:",
-        "screen_textinput_prompt": "Як назвається наступна область?",
+        #"screen_textinput_title": "Відгадай область:",
+        #"screen_textinput_prompt": "Як назвається наступна область?",
         "phrase_to_finish_the_game": "Вихід",
-        "medium_lvl_textinput_entry_name_of_region": "Спроб залишилось/всього: ",
-        "medium_lvl_textinput_prompt:": "Введіть назву вибраної області:"
+        #"medium_lvl_textinput_entry_name_of_region": "Спроб залишилось/всього: ",
+        "medium_lvl_textinput_prompt:": "Введіть назву вибраної області:",
+        "lbl_to_entry_field_text": "Введіть назву області:",
+        "lbl_to_choose_difficulty_text":"Виберіть складність:",
+        "easy":"легка",
+        "medium":"середня",
+        "hard":"важка",
     },
     "en":{
         "screen_title": "Ukraine Oblasts Game",
         "division_name": "en_name",
-        "screen_textinput_title":"Guess the oblast",
-        "screen_textinput_prompt":"What's another oblast's name?",
         "phrase_to_finish_the_game": "Exit",
-        "medium_lvl_textinput_entry_name_of_region": "Attempts left / total:",
-        "medium_lvl_textinput_prompt:": "Enter the name of the selected region:"
+        "medium_lvl_textinput_prompt:": "Enter the name of the selected region:",
+        "lbl_to_entry_field_text": "Entry a region name:",
+        "lbl_to_choose_difficulty_text":"Choose difficulty:",
+        "easy": "easy",
+        "medium": "medium",
+        "hard": "hard",
+
     }
 }
