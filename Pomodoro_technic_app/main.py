30343c33287aa7b6a1a364ec763a415bca4a89d0
Y-K27 <yuriikarpiuk21@gmail.com>
2025-07-17 15:26:47 +0200

    Created a function to count cycles and automatically switch between worktime and break time in a 2-hour cycle

--- Pomodoro_technic_app/main.py
+++ Pomodoro_technic_app/main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-import time
+from cgitb import enable
+from idlelib import window
+
 # Constants
 #--colors
 PINK = "#FFA27F"
@@ -12,8 +14,11 @@ FONT_NAME = "Courier-Bold"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+cycle_count = 0
 
 # Time reset
+def timer_reset():
+    pass
 
 #Timer
 def start_timer():
@@ -21,8 +26,23 @@ def start_timer():
     :return: nothing
     call count_down() and set value of start time for timer
     '''
-    count_down(5*60)
-
+    global cycle_count
+    cycle_count += 1
+    print(cycle_count)
+    if cycle_count == 0 or cycle_count % 2 == 1:
+        work_cycle_marker_label['text'] = "WORK"
+        work_cycle_marker_label['fg'] = GREEN
+        checkmark_label['text'] = (cycle_count//2)* "✔"
+        count_down(WORK_MIN*60)
+    elif cycle_count<7 and cycle_count % 2 == 0:
+        work_cycle_marker_label['text'] = "BREAK"
+        work_cycle_marker_label['fg'] = PINK
+        count_down(SHORT_BREAK_MIN*60)
+    elif cycle_count == 8:
+        work_cycle_marker_label['text'] = "BREAK"
+        work_cycle_marker_label['fg'] = RED
+        checkmark_label['text'] = (cycle_count // 2) * "✔"
+        count_down(LONG_BREAK_MIN*60)
 #Countdown
 def count_down(count):
     '''
@@ -30,6 +50,8 @@ def count_down(count):
     :return: nothing
     set timer label of timer and change it every 1000 milliseconds
     '''
+    global cycle_count
+    start_btn['state'] = tk.DISABLED
     count_min = count // 60
     count_sec = count % 60
     if count_min < 10:
@@ -40,6 +62,16 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=time_set_text)
     if count > 0:
         timer_window.after(1000, count_down, count-1)
+    if count == 0:
+        if cycle_count < 8:
+            start_timer()
+        else:
+            cycle_count =0
+            checkmark_label['text'] = ""
+            work_cycle_marker_label['text'] = ""
+            start_btn['state'] = tk.NORMAL
+
+
 
 #UI
 #-- window settings
@@ -58,14 +90,17 @@ canvas.grid(column=1, row=1)
 
 
 #-- widgets:
-checkmark_label = tk.Label(timer_window, text="✔",font=(FONT_NAME, 15,"bold"), fg=GREEN, bg=YELLOW)
+work_cycle_marker_label = tk.Label(timer_window, text="",font=(FONT_NAME, 20,"bold"),bg=YELLOW)
+work_cycle_marker_label.grid(column=1, row=0)
+
+checkmark_label = tk.Label(timer_window, text="",font=(FONT_NAME, 15,"bold"), fg=GREEN, bg=YELLOW)
 checkmark_label.grid(column=1, row=3)
 
 start_btn = tk.Button(timer_window, text="START", command=start_timer)
 start_btn.config(width=7, bg=PINK, fg='white',highlightthickness=0, font=(FONT_NAME, 12,"bold"), borderwidth=1)
 start_btn.grid(column=1, row=2, sticky=tk.W)
 
-reset_btn = tk.Button(timer_window, text="RESET", command=timer_window.destroy)
+reset_btn = tk.Button(timer_window, text="RESET", command=timer_reset)
 reset_btn.config(width=7, bg=PINK, fg='white',highlightthickness=0, font=(FONT_NAME, 12,"bold"), borderwidth=0)
 reset_btn.grid(column=1, row=2,sticky=tk.E)
 
